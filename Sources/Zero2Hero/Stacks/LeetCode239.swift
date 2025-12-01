125551b46816bd4e6d04fa678f0a733072286cf6
Manoj Tudu <manoj.tudu@travclan.com>
2025-12-01 12:56:57 +0530

    LeetCode 239 - Sliding window maximum array added

--- Sources/Zero2Hero/Stacks/LeetCode239.swift
+++ Sources/Zero2Hero/Stacks/LeetCode239.swift
@@ -1,10 +1,40 @@
 class LeetCode239 {
+    // [1, 2, 10, 50, 90, 100]
+    // 1, 2, 3  -> 3 
+    // 2, 3, 10 -> 10
+    // Decreasing Monotonic Stack
+    // We will also keep a track of stack front so that
+    // when window is moved we ensure that we are not referencing to any values 
+    // that are currently not under scope/window
+    static func slidingWindowMaximum(_ arr:[Int], _ k:Int) -> [Int] {
+        var iStack:[Int] = [Int]()
+        var result: [Int] = [Int]()
+        var stackFront = 0
 
+        for i:Int in 0..<arr.count{
+            let windowStartIndex = i - k + 1    
 
+            if stackFront < iStack.count, iStack[stackFront] < windowStartIndex {
+                stackFront += 1
+            }
 
-    // ----------------------------
-    // ||  Brute Force Method   ||
-    // ----------------------------
+            while iStack.count > stackFront, arr[iStack.last!] < arr[i] {
+                iStack.removeLast()
+            }
+            iStack.append(i)
+
+            if i + 1 >= k {
+                let maxArrIndex =  iStack[stackFront]
+                result.append(arr[maxArrIndex])
+            }
+        }
+
+        return result
+    }
+
+    // ---------------------------
+    // |   Brute Force Method    |
+    // ---------------------------
     // static func slidingWindowMaximum() {
     //     let inputArr = [-1, -5, -3, -4, -6, 0, -8]
     //     let k: Int = 3
@@ -16,7 +46,18 @@ class LeetCode239 {
     //         }
     //         resultArr.append(maxWindowVal)
     //     }
-
     //     print(resultArr)
-    // }     
+    // }   
 }
+/*
+    0, 1, 2, ..., n-1
+    i, i+1, i+2, i, i+1, i+2 ... i + k - 1 
+    so that it never goes out of bounds
+    i + k - 1 < n - 1
+    i < n - k
+
+    0, 1, 2, 3 ... n - k
+    => (n - k) - (0) + 1 
+    => n - k + 1
+*/
+
--- Sources/Zero2Hero/Zero2Hero.swift
+++ Sources/Zero2Hero/Zero2Hero.swift
@@ -1,11 +1,12 @@
 @main
 struct Zero2Hero {
     static func main() -> Void {
-       print("Largest Rectangle Histogram: ",LeetCode81.largestRectangleHistogram([1, 10,3]))
-       print("Next Warmest Day: ",LeetCode789.nextWarmestDay([73,74,75,71,69,72,76,73]))
-       let input =  "([])("
-       print("\(input) -> Is Valid Parantheses: ", LeetCode20.isValidParantheses(input))
-       print("Simplified Canonical Path: ", LeetCode71.simplifiedCanonicalPath("/home/user/Documents/../Pictures"))
-       print("Sliding window sum: ", LeetCodeXX1.slidingWindowSum([1, 2, 10, 50, 90, 100], 3))
+    //    print("Largest Rectangle Histogram: ",LeetCode81.largestRectangleHistogram([1, 10,3]))
+    //    print("Next Warmest Day: ",LeetCode789.nextWarmestDay([73,74,75,71,69,72,76,73]))
+    //    let input =  "([])("
+    //    print("\(input) -> Is Valid Parantheses: ", LeetCode20.isValidParantheses(input))
+    //    print("Simplified Canonical Path: ", LeetCode71.simplifiedCanonicalPath("/home/user/Documents/../Pictures"))
+    //    print("Sliding window sum: ", LeetCodeXX1.slidingWindowSum([1, 2, 10, 50, 90, 100], 3))
+       print("Sliding window max: ", LeetCode239.slidingWindowMaximum([1,3,-1,-3,5,3,6,7], 3))
     }
 }
